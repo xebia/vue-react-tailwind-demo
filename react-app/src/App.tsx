a7621f1ce776a0ed7bd06d2191f160b20b18ac09
Frank van Wijk <fvanwijk@xebia.com>
2020-09-13 17:04:59 +0200

    Add Tailwind to React

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "twin.macro";
 import Button from "./Button";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <h1>React example</h1>
+      <h1 tw="text-4xl font-bold mb-4">React example</h1>
       <Button>Button</Button>
     </div>
   );
--- react-app/src/Button.tsx
+++ react-app/src/Button.tsx
@@ -1,6 +1,10 @@
 import React, { FC } from "react";
-import styles from "./Button.module.css";
+import "twin.macro";
 
-const Button: FC<{}> = ({ children }) => <button className={styles.button}>{children}</button>;
+const Button: FC<{}> = ({ children }) => (
+  <button tw="bg-primary hover:bg-primary-dark md:bg-secondary md:hover:bg-secondary-dark text-white border-0 rounded px-3 py-2 cursor-pointer transition-colors duration-300">
+    {children}
+  </button>
+);
 
 export default Button;
